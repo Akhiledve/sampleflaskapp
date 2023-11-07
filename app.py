61ead6537649ab115e3235a8d460827646e52a95
Akhiledve <140639540+Akhiledve@users.noreply.github.com>
2023-11-07 10:19:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+Johnson Meitei9:55 AM
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+# Sample data for tasks
+tasks = []
+
+@app.route('/service_1', methods=['GET', 'POST'])
+def manage_tasks():
+    if request.method == 'GET':
+        # Return a list of tasks
+        return jsonify({'tasks': tasks})
+    elif request.method == 'POST':
+        # Create a new task
+        data = request.get_json()
+        task = {
+            'id': len(tasks) + 1,
+            'title': data['title'],
+            'done': F
+duu-wxik-usr
